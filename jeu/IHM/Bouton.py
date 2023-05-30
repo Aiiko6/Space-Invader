7fee6ba601e87bc7e5ea2a7fbbbcf5d48dec26b3
p1905199 <aiikoffs6@gmail.com>
2023-05-30 15:58:25 +0200

    Page Graphique

--- jeu/IHM/Bouton.py
+++ jeu/IHM/Bouton.py
@@ -2,10 +2,12 @@ import core
 
 
 class Bouton:
-    def __init__(self,coordX,coordY):  # constructeur
+    def __init__(self,coordX,coordY, Visible=True):  # constructeur
+        self.visible = Visible
         self.couleur = (255,255,255)
         self.position = (coordX, coordY)
         self.mass = 20
 
     def show(self):
-        core.Draw.circle(self.couleur, self.position, self.mass)
+        if self.visible:
+            core.Draw.circle(self.couleur, self.position, self.mass)
---
+++ jeu/IHM/PageGraphique.py
@@ -0,0 +1,60 @@
+import pygame
+from pygame import Vector2
+
+import core
+from jeu.Etat import Etat
+from jeu.IHM.Bouton import Bouton
+
+
+class PageGraphique:
+    def __init__(self):  # constructeur
+        self.couleur = (255, 255, 255)
+        self.mouse = pygame.mouse.get_pos()
+        self.massCursor = 2
+        self.startMenu = 1
+        self.Fullscreen = False
+        self.bp = Bouton(300, 500)
+        self.bpFullNo = Bouton(520, 210, False)
+        self.bpFullYes = Bouton(520, 210, False)
+
+    def update(self):
+
+        self.mouse = pygame.mouse.get_pos()
+        core.Draw.text(self.couleur, 'Graphique :', (300, 10))
+
+        core.Draw.text(self.couleur, 'Fullscreen :', (300, 200))
+
+        self.bpFullNo.show()
+        self.bpFullYes.show()
+
+        if self.Fullscreen:
+            core.Draw.text(self.couleur, 'No', (500, 200))
+        else:
+            core.Draw.text(self.couleur, 'Yes', (500, 200))
+
+        self.bp.show()
+        core.Draw.text(self.couleur, 'Retour ', (340, 480))
+
+        if (self.startMenu == 0) or (not core.getMouseLeftClick()):
+            self.startMenu = 0
+
+            if core.getMouseLeftClick() and self.distanceCheck(self.bpFullNo) and self.Fullscreen:
+                self.startMenu = 1
+                self.Fullscreen = False
+
+            if core.getMouseLeftClick() and self.distanceCheck(self.bpFullYes) and not self.Fullscreen:
+                self.startMenu = 1
+                self.Fullscreen = True
+
+            if core.getMouseLeftClick() and self.distanceCheck(self.bp):
+                self.startMenu = 1
+                core.memory('etat', Etat.OPTION)
+
+    def distanceCheck(self, bouton):
+        pos1 = Vector2(self.mouse)
+        pos2 = Vector2(bouton.position)
+        distance = pos1 - pos2
+        if self.massCursor + bouton.mass > distance.length():
+            return True
+        else:
+            return False
